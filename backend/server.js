e67ea2a3c269631be6c9b68d2d993487f3178604
Jimnora <djimnez@uc.cl>
2024-05-09 22:25:03 -0400

    feat: fix methods and create filter for get notes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { initializeApp } = require('firebase/app');
-const { getFirestore, doc, setDoc, deleteDoc } = require('firebase/firestore');
+const { getFirestore, doc, getDoc, getDocs, updateDoc, deleteDoc, collection, addDoc, serverTimestamp } = require('firebase/firestore');
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -41,63 +42,148 @@ app.post('/notebooks', async (req, res) => {
             name: name,
         };
 
-        //const checkNotebooksRef = await db.collection('notebooks').where('name', '==', name).get();
+        // check if the name is empty
+        if (!data.name) {
+            return res.status(400).json({ error: 'Name is required' });
+        }
 
-        //if (!checkNotebooksRef.empty) {
-        //    return res.status(400).json({ error: 'Notebook name already exists' });
-        //}
+        // check if notebook already exists
+        const notebooksRef = collection(db, 'notebooks');
+        const querySnapshot = await getDocs(notebooksRef);
+        const notebooks = querySnapshot.docs.map(doc => doc.data());
 
-        const notebookRef = doc(db, 'notebooks', '1');
-        let notebook = await setDoc(notebookRef, data);
-        
-        res.json(data);
+        if (notebooks.some(notebook => notebook.name === data.name)) {
+            return res.status(400).json({ error: 'Notebook already exists' });
+        }
 
+        // create new notebook
+        const docRef = await addDoc(notebooksRef, data);
+
+        res.json({ id: docRef.id, ...data });
     } catch (err) {
         res.status(500).json({ error: 'Failed to create notebook' });
     }
 });
 
-// TO DO: update other methods
+app.delete('/:notebook_id', async (req, res) => {
+    try {
+        const { notebook_id } = req.params;
+
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+
+        await deleteDoc(notebookRef);
+
+        res.json({ message: 'Notebook deleted' });
+
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to delete notebook' });
+    }
+});
 
 // Define API routes for notes
-app.put('/notes/:id', async (req, res) => {
+app.put('/:notebook_id/notes/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        const { title, content } = req.body;
 
-        const noteRef = db.collection('notes').doc(id);
-        await noteRef.update({ title, content });
+        const { notebook_id, id } = req.params;
 
-        res.json({ id, title, content });
+        const { content, checked, color} = req.body;
+
+        const data = {
+            content: content,
+            checked: checked,
+            color: color,
+        };
+
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+        const docSnap = await getDoc(notebookRef);
+
+        if (!docSnap.exists()) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+
+        const notesRef = collection(notebookRef, 'notes');
+        const noteRef = doc(notesRef, id);
+        await updateDoc(noteRef, data);
+
+        res.json({ id, ...data });
+        
     } catch (err) {
+        console.log(err)
         res.status(500).json({ error: 'Failed to update note' });
     }
 });
 
-app.delete('/notes/:id', async (req, res) => {
+app.delete('/:notebook_id/notes/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        
-        const noteRef = db.collection('notes').doc(id);
-        await noteRef.delete();
+
+        const { notebook_id, id } = req.params;
+
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+        const docSnap = await getDoc(notebookRef);
+
+        if (!docSnap.exists()) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
+
+        const notesRef = collection(notebookRef, 'notes');
+        const noteRef = doc(notesRef, id);
+        await deleteDoc(noteRef);
 
         res.json({ message: 'Note deleted' });
+
     } catch (err) {
         res.status(500).json({ error: 'Failed to delete note' });
     }
 });
 
+function filterNotes(filters, notes) {
+    let filteredNotes = notes;
+    if (!filters) {
+        return filteredNotes;
+    }
+    if (filters.color) {
+        filteredNotes = filteredNotes.filter(note => note.color === filters.color);
+    }
+    if (filters.checked) {
+        const checked = filters.checked === 'true';
+        filteredNotes = filteredNotes.filter(note => note.checked === checked);
+    }
+    if (filters.orderBy) {
+        //order from a-z or z-a based on content
+        filteredNotes = filteredNotes.sort((a, b) => {
+            if (filters.orderBy === 'a-z') {
+                return a.content.localeCompare(b.content);
+            }
+            return b.content.localeCompare(a.content);
+        });
+    }
+    return filteredNotes;
+}
+
+// dynamic route to fetch notes of a notebook
 app.get('/:notebook_id/notes', async (req, res) => {
     try {
+        
         const { notebook_id } = req.params;
+        const filters = req.query;
 
-        const noteRef = db.collection('notes').where('notebook_id', '==', notebook_id).get();
-        const notes = noteRef.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-        }));
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+        const docSnap = await getDoc(notebookRef);
 
-        res.json(notes);
+        if (!docSnap.exists()) {
+            // if notebook not found, return empty array
+            return res.json([]);
+        }
+
+        const notesRef = collection(notebookRef, 'notes');
+        const notesSnapshot = await getDocs(notesRef);
+        const notes = notesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        
+        // filter notes based on query parameters
+        const filteredNotes = filterNotes(filters, notes);
+        
+        res.json(filteredNotes);
+
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch notes' });
     }
@@ -106,20 +192,35 @@ app.get('/:notebook_id/notes', async (req, res) => {
 app.post('/:notebook_id/notes', async (req, res) => {
     try {
         const { notebook_id } = req.params;
-        const { title, content } = req.body;
 
-        const noteRef = await db.collection('notes').add({
-            title,
-            content,
-            notebook_id
-        });
+        const { content, checked, color} = req.body;
+
+        const data = {
+            content: content,
+            checked: checked,
+            color: color,
+            createdAt: serverTimestamp(),
+        };
+
+        // create new note inside collection notes inside the notebook
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+        const docSnap = await getDoc(notebookRef);
+
+        if (!docSnap.exists()) {
+            return res.status(404).json({ error: 'Notebook not found' });
+        }
         
-        res.json({
-            id: noteRef.id,
-            title,
-            content,
-            notebook_id
+        const notesRef = collection(notebookRef, 'notes');
+    
+        const newNoteRef = await addDoc(notesRef, data);
+
+        res.json({ 
+            id: newNoteRef.id,
+            content: data.content,
+            checked: data.checked,
+            color: data.color
         });
+            
         
     } catch (err) {
         res.status(500).json({ error: 'Failed to create note' });
@@ -130,26 +231,25 @@ app.get('/:notebook_id', async (req, res) => {
     try {
         const { notebook_id } = req.params;
 
-        const notebookDoc = await db.collection('notebooks').doc(notebook_id).get();
+        const notebookRef = doc(db, 'notebooks', notebook_id);
+        const docSnap = await getDoc(notebookRef);
 
-        if (!notebookDoc.exists) {
+        if (!docSnap.exists()) {
             return res.status(404).json({ error: 'Notebook not found' });
         }
 
-        res.json({
-            id: notebookDoc.id,
-            ...notebookDoc.data()
-        });
-
+        const notebook = docSnap.data();
+        res.json(notebook);
+        
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch notebook' });
+        console.log(err);
     }
 });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 
 // Start the server
 app.listen(port, () => {
